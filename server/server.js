03faf870b66af0e4f20042ef3f8493d370f15c2f
m8601 <robert.szymanski08@gmail.com>
2017-12-20 12:19:21 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+  //GET todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
 // Port
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
