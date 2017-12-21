4ec101ae71e3b23fa95331af67a6cd4fdce396ce
m8601 <robert.szymanski08@gmail.com>
2017-12-21 16:31:07 +0100

    bug fixes

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
+
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || 'test' ) {
+if (env === 'development' || env === 'test') {
   var config = require('./config.json');
   var envConfig = config[env];
 
